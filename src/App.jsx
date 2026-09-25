45fd57648ad61cba4edc66527316935bde9313b1
Wojciech <venomlizard2007@gmail.com>
2026-09-25 09:27:43 +0200

    Added navbar component and container to store photo components more easily

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import PhotoCard from "../components/PhotoCard"
+import Navbar from "../components/Navbar"
 
 function App()
 {
   return(
     <>
-      <PhotoCard />
+    <Navbar />
+      <div className="container mt-4">
+        <h1>Galeria zdjęć</h1>
+        <PhotoCard />
+      </div>
     </>
   )
 }
